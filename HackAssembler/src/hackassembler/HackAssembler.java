1247b694195fb4dbf0fb8fb2a42b9615aa96f310
Mynor Xico <mynorottoniel.xico2@gmail.com>
2018-03-15 08:14:58 -0600

    Parser (Not tested yet)

--- HackAssembler/src/hackassembler/HackAssembler.java
+++ HackAssembler/src/hackassembler/HackAssembler.java
@@ -5,6 +5,7 @@
  */
 package hackassembler;
 
+import hackassembler.Parser.instructionType;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -23,19 +24,37 @@ public class HackAssembler {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-        Path inputPath = Paths.get("C:\\Users\\Maynor\\Desktop", "Prog.asm");
+        String input = "C:\\Users\\Maynor\\Desktop\\Prog.asm";
         
-        List<String> lines;
-        lines = Files.readAllLines(inputPath);
+        Parser p = new Parser(input);
         
-        for(String line : lines){
-            System.out.println(line);
+        while(true){
+            p.advance();
+            if(p.commandType() == instructionType.C_COMMAND){
+                System.out.println("TIPO C");
+                System.out.println("Dest: " + p.dest());
+                System.out.println("Jump: " + p.jump());
+                System.out.println("Comp: " + p.comp());
+                System.out.println("111" +binaryFixedLength(Code.comp(p.comp()), 7)+binaryFixedLength(Code.dest(p.dest()), 3)+binaryFixedLength(Code.jump(p.jump()),3));
+                System.out.println("---------------------------");            
+            }else{
+                System.out.println("TIPO A");
+                System.out.println(p.currentCommand);
+                System.out.println("Symbol: " + p.symbol());
+                System.out.println("---------------------------");  
+            }
+            
         }
         
         
-        
-        Path outputPath = Paths.get("C:\\Users\\Maynor\\Desktop", "Prog.hack");
-        Files.write(outputPath, lines);
+        //Path outputPath = Paths.get("C:\\Users\\Maynor\\Desktop", "Prog.hack");
+        //Files.write(outputPath, lines);
+    }
+    static String fixedLengthString(String string, int length){
+            return String.format("%1$" + length+"s",string).replace(' ', '0');
+    }
+    static String binaryFixedLength(int number, int length){
+        return fixedLengthString(Integer.toString(number, 2), length);
     }
     
 }
--- HackAssembler/src/hackassembler/Parser.java
+++ HackAssembler/src/hackassembler/Parser.java
@@ -5,30 +5,50 @@
  */
 package hackassembler;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
 
 /**
  *
  * @author Maynor
  */
 public class Parser {
+    BufferedReader br;
     
+    String currentCommand;
+    String currentLine;
     // Opens the input file/stream and gets ready to parse it
-    public Parser(String inputPath){
-        
+    public Parser(String inputPath) throws FileNotFoundException{
+        currentCommand = null;
+        currentLine = null;
+        br = new BufferedReader(new FileReader(inputPath));
     }
     
     // Arethere more commands in the input
-    boolean hasMoreCommands(){
-        return false;
+    boolean hasMoreCommands() throws IOException{
+        currentLine = br.readLine();
+        if(currentLine == null){
+            br.close();
+            return false;
+        }else{
+            return true;
+        }
     }
     
     // Reads the next command from the input and makes it the current command.
     // Should be called only if hasMoreCommands. Initially there is no current
     // command    
-    void advance(){
+    void advance() throws IOException{
+        if(hasMoreCommands()){
+            if(!currentLine.equals("") && !currentLine.startsWith("//")){
+                currentCommand = currentLine.trim();
+            }else{
+                advance();
+            }
+        }
     }
     
     // Returns the type of the current command
@@ -38,31 +58,61 @@ public class Parser {
         L_COMMAND for (Xxx)where Xxx is a symbol
     */
     instructionType commandType(){
-        return null;
+        if(currentCommand.substring(0, 1).equals("@")){
+            return instructionType.A_COMMAND;
+        }else if(currentCommand.substring(0, 1).equals("(")){
+            return instructionType.L_COMMAND;
+        }else{
+            return instructionType.C_COMMAND;
+        }
     }
     
-    // Returns the symbol or decimal Xxx of the current command @Xxx or (Xxx(.
+    // Returns the symbol or decimal Xxx of the current command @Xxx or (Xxx)
     // Should be called only when commandType() is A_COMMAND OR L_COMMAND.
     String symbol(){
-        return null;        
+        switch(commandType()){
+            case A_COMMAND:
+                return currentCommand.substring(1);
+            case L_COMMAND:
+                return currentCommand.replace('(', ' ').replace(')', ' ').trim();
+            default:
+                return null;
+        }
     }
     
     // Returns the dest mnemonic in the current C-command (8 posibilities).
     // Should be called only when commandType() is C_COMMAND.
     String dest(){
-        return null;        
+        if(commandType() == instructionType.C_COMMAND){
+            if(currentCommand.contains("=")){
+                return currentCommand.substring(0,1);
+            }else{
+                return "null";
+            }
+        }
+        return null;
     }
     
     // Returns the comp mnemonic in the current C-command (28 posibilities). 
     // Should be called only when commandType() is C_COMMAND
     String comp(){
-        return null;
+        if(commandType() == instructionType.C_COMMAND){
+            if(!currentCommand.contains(";")){
+                return currentCommand.substring(2);
+            }else{
+                return currentCommand.substring(0, 1);
+            }            
+        }
+        return "null";
     }
     
     // Returns the jump mnemonic in the current C-command (8 posibilities). 
     // Should be called only when commandType() is C_COMMAND.
     String jump(){
-        return null;
+        if(currentCommand.contains(";")){
+            return currentCommand.substring(2);
+        }
+        return "null";
     }
     
     
